e043dfaeeacfce4e38568176cb3364f3c50aa252
Praveen KP <praveenprathap.ekm@gmail.com>
2020-08-03 15:51:15 +0530

    background image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    hello
+    <main>
+      <div class="search-box">
+        <input type="text"  class="search-bar" placeholder="Search...">
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Inida, In</div>
+          <div class="date">Monday 03 2020</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -8,10 +18,68 @@
 
 export default {
   name: 'App',
+  data (){
+    return {
+      api_key: '0526751d4e126ef3c638c92e46c6534b'
+    }
+  }
  
 }
 </script>
 
 <style>
 
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'montserrat',sans-serif ;
+}
+
+#app{
+  background-image: url('./assets/normalbg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main{
+  min-height: 120vh;
+  padding: 25px;
+  
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25),rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border:none;
+  outline: none;
+  background:none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0,0.25);
+  background-color:  rgba(255, 255, 225, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0,0.25);
+  background-color: rgba(255, 255, 225, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
 </style>
