28132afc908b07f2c0ea64d636fd5dd382b136b4
Praveen KP <praveenprathap.ekm@gmail.com>
2020-08-03 16:17:46 +0530

    texture style updated

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
       <div class="weather-wrap">
         <div class="location-box">
           <div class="location">Inida, In</div>
-          <div class="date">Monday 03 2020</div>
+          <div class="date">Monday 03 August 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9ºc</div>
+          <div class="weather">Rain</div>
         </div>
       </div>
     </main>
@@ -81,5 +85,48 @@ main{
   box-shadow: 0px 0px 16px rgba(0, 0, 0,0.25);
   background-color: rgba(255, 255, 225, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color:#fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0,0.25);
+}
+
+.location-box .date{
+  color:#fff;
+  font-size: 20px;
+  font-weight: 500;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box{
+  text-align: center;
+}
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0,0.25);
+  background-color: rgba(255, 255, 225, 0.25);
+  border-radius:16px;
+  margin:3px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0,0.25);
+}
+
+.weather-box .weather{
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0,0.25);
 }
 </style>
